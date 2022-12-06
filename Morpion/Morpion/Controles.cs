d84231f62aead9d14ce76245fc57b559684a75ab
Florian Coulange <florian.coulange@gmail.com>
2022-12-06 13:52:13 +0100

    Ajout du controle de victoire et creation d'un menu basic

--- Morpion/Morpion/Controles.cs
+++ Morpion/Morpion/Controles.cs
@@ -27,5 +27,48 @@ namespace Morpion
             }
             return false;
         }
+        static public bool TestVictoire(int[,] grille, int Numero)
+        {
+            int i;
+            if ((grille[0,0] == Numero) && (grille[1,1] == Numero) && (grille[2,2] == Numero))
+            {
+                return true;
+            }
+            if ((grille[0,2] == Numero) && (grille[1,1] == Numero) && (grille[2,0] == Numero))
+            {
+                return true;
+            }
+            for (i = 0; i < 3; i++)
+            {
+                if ((grille[i,0] == Numero) && (grille[i,1] == Numero) && (grille[i,2] == Numero))
+                {
+                    return true;
+                }
+            }
+            for (i = 0; i < 3; i++)
+            {
+                if ((grille[0,i] == Numero) && (grille[1,i] == Numero) && (grille[2,i] == Numero))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static public bool TestEgalite(int[,] grille)
+        {
+            int x, y;
+            for (y = 0; y < 3; y++)
+            {
+                for (x = 0; x < 3; x++)
+                {
+                    if (grille[y,x] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
     }
 }
--- Morpion/Morpion/Program.cs
+++ Morpion/Morpion/Program.cs
@@ -21,43 +21,71 @@ namespace Morpion
             Joueur[] joueurs = new Joueur[2] {joueur1, joueur2};
             int joueurEnCours = 0;
 
-            int[,] grilleMorpion = new int[3, 3] { { 1, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
-
-            bool partie = true;
-            while(partie == true)
+            int[,] grilleMorpion = new int[3, 3] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            Affichage.Afficher(grilleMorpion, joueurs[joueurEnCours]);
+            
+            bool jeu = true;
+            while(jeu == true)
             {
-                bool tour = false;
-                Affichage.Afficher(grilleMorpion, joueurs[joueurEnCours]);
-                while (tour == false)
+                
+                bool partie = false;
+                Console.Write($"Pour jouer tapez 'Oui' : ");
+                if (Console.ReadLine() == "Oui")
                 {
-                    
-                    joueurs[joueurEnCours].AppuiTouche();
-                    tour = Controles.VerifTouche(grilleMorpion, joueurs[joueurEnCours].Touche);
-                    if(tour == true)
-			        {
-                        int x, y, compteurTouche = 0;
-                        for (y = 0; y < 3; y++)
+                    partie = true;
+                    grilleMorpion = new int[3, 3] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+                
+                while(partie == true)
+                {
+                    bool tour = true;
+                    Affichage.Afficher(grilleMorpion, joueurs[joueurEnCours]);
+                    while (tour == true)
+                    {
+                        joueurs[joueurEnCours].AppuiTouche();
+                        if(Controles.VerifTouche(grilleMorpion, joueurs[joueurEnCours].Touche) == true)
                         {
-                            for (x = 0; x < 3; x++)
+                            int x, y, compteurTouche = 0;
+                            for (y = 0; y < 3; y++)
                             {
-                                compteurTouche++;
-                                if (compteurTouche == joueurs[joueurEnCours].Touche)
+                                for (x = 0; x < 3; x++)
                                 {
-                                    grilleMorpion[y,x] = joueurs[joueurEnCours].Numero;
+                                    compteurTouche++;
+                                    if (compteurTouche == joueurs[joueurEnCours].Touche)
+                                    {
+                                        grilleMorpion[y,x] = joueurs[joueurEnCours].Numero;
 
-
-                                    //Changement de joueur
-                                    joueurEnCours = (joueurEnCours == 0 ? 1 : 0);
+                                        if (Controles.TestVictoire(grilleMorpion, joueurs[joueurEnCours].Numero) == true)
+                                        {
+                                            Affichage.Afficher(grilleMorpion, joueurs[joueurEnCours]);
+                                            Console.Write("Victoire de " + joueurs[joueurEnCours].Name + " !!!\n\n");
+                                            partie = false;
+                                        }
+                                        if (Controles.TestEgalite(grilleMorpion) == true)
+                                        {
+                                            Affichage.Afficher(grilleMorpion, joueurs[joueurEnCours]);
+                                            Console.Write("Egalit\x82\\e...\n");
+                                            partie = false;
+                                        }
+                                    }
                                 }
                             }
+
+                            //Changement de joueur
+                            joueurEnCours = (joueurEnCours == 0 ? 1 : 0);
+                            tour = false;
                         }
-                    }
-                    else
-                    {
-                        Console.WriteLine("La touche est invalide");
+                        else
+                        {
+                            Console.WriteLine("La touche est invalide");
+                        }
                     }
                 }
             }
         }
     }
-}
+ }
